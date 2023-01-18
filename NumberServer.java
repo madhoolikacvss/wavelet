ef1b6f98365ec84ff6f47aa0d710921bc5b0f5a1
madhoolikacvss <madhoolika4@gmaiol.com>
2023-01-18 12:06:42 -0800

    Update NumberServer.java

--- NumberServer.java
+++ NumberServer.java
@@ -17,7 +17,7 @@ class Handler implements URLHandler {
             if (url.getPath().contains("/add")) {
                 String[] parameters = url.getQuery().split("=");
                 if (parameters[0].equals("count")) {
-                    num += Integer.parseInt(parameters[1]);
+                    num += Integer.parseInt(parameters[1111]);
                     return String.format("Number increased by %s! It's now %d", parameters[1], num);
                 }
             }
